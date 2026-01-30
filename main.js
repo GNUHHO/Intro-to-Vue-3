0661ddf7a0f648643f1d45fe7b5e71c7164549ab
GNUHHO <hothehung169a8nh1920@gmail.com>
2026-01-30 11:47:44 +0700

    updated

--- main.js
+++ main.js
@@ -4,7 +4,14 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            //Lesson 5
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green' },
+                { id: 2235, color: 'blue' }
+            ],
+            //Lesson 5 challenge
+            size: [ 'S', 'M', 'L', 'XL' ]
         }
     }
 })
