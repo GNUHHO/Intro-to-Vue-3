cf5bd0a5293ee7da44495b014e71538d411682fd
GNUHHO <hothehung169a8nh1920@gmail.com>
2026-02-06 10:29:32 +0700

    updated

---
+++ components/ProductDetail.js
@@ -0,0 +1,14 @@
+//Lesson 9 Challenge: add Product Details Component
+app.component('product-detail', {
+    props: {
+        details: {
+            type: Array,
+            required: true
+        }
+    },
+    template: `
+    <ul>
+      <li v-for="detail in details">{{ detail }}</li>
+    </ul>
+    `
+});
